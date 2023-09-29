008285b5984f2fc6135f043879a921f67c9f7b37
DKamalie <46668318+DKamalie@users.noreply.github.com>
2023-09-29 05:57:41 +0200

    Menu update

--- src/main/java/com/example/application/factory/BaseFactory.java
+++ src/main/java/com/example/application/factory/BaseFactory.java
@@ -7,16 +7,16 @@ import com.example.application.util.Helper;
  Author: Timothy Lombard (220154856)
  Date: 30th July (last updated) 2023
 */
-public class BaseFactory {
-
-    public static Base buildBase(Base.BaseCrust crust, Base.BaseThickness thickness, Base.BaseTexture texture, double price){
-        if(Helper.isNullOrEmpty(String.valueOf(crust)) || Helper.isNullOrEmpty(String.valueOf(thickness)) || Helper.isNullOrEmpty(String.valueOf(texture)) || Helper.isNullOrEmpty(String.valueOf(price))){
-            return null;
-        }
-
-        String baseId = Helper.generateId();
-
-        Base b = new Base.Builder().setBaseId(baseId).setCrust(crust).setThickness(thickness).setTexture(texture).setPrice(price).build();
-        return b;
-    }
-}
+//public class BaseFactory {
+//
+//    public static Base buildBase(Base.BaseCrust crust, Base.BaseThickness thickness, Base.BaseTexture texture, double price){
+//        if(Helper.isNullOrEmpty(String.valueOf(crust)) || Helper.isNullOrEmpty(String.valueOf(thickness)) || Helper.isNullOrEmpty(String.valueOf(texture)) || Helper.isNullOrEmpty(String.valueOf(price))){
+//            return null;
+//        }
+//
+//        String baseId = Helper.generateId();
+//
+//        Base b = new Base.Builder().setBaseId(baseId).setCrust(crust).setThickness(thickness).setTexture(texture).setPrice(price).build();
+//        return b;
+//    }
+//}
--- src/main/java/com/example/application/factory/OrderFactory.java
+++ src/main/java/com/example/application/factory/OrderFactory.java
@@ -13,34 +13,34 @@ import java.time.LocalTime;
  Date: 30th July (last updated) 2023
 */
 
-    public class OrderFactory {
-        /*
-
-        public static Order buildOrder(LocalDate createDate, LocalTime time, Customer customer, Order.OrderStatus orderStatus, Pizzeria pizzeria) {
-            if (Helper.isNullOrEmpty(String.valueOf(Helper.isNullOrEmpty(String.valueOf(createDate)))) || Helper.isNullOrEmpty(String.valueOf(time))  || Helper.isNullOrEmpty(String.valueOf(customer)) || Helper.isNullOrEmpty(String.valueOf(orderStatus)) || Helper.isNullOrEmpty(String.valueOf(pizzeria)))  {
-                return null;
-            }
-
-            String orderId = Helper.generateId();
-
-            Order order = new Order.Builder().setOrderId(orderId).setCreateDate(createDate).setTime(time).setCustomer(customer).setOrderStatus(orderStatus).setPizzeria(pizzeria).build();
-            return order;
-
-        }
-
-         */
-
-        public static Order createOrder(LocalDate createDate, LocalTime time, Customer customer, Order.OrderStatus orderStatus, Pizzeria pizzeria) {
-            if (Helper.isNullOrEmpty(String.valueOf(Helper.isNullOrEmpty(String.valueOf(createDate)))) || Helper.isNullOrEmpty(String.valueOf(time))  || Helper.isNullOrEmpty(String.valueOf(customer)) || Helper.isNullOrEmpty(String.valueOf(orderStatus)) || Helper.isNullOrEmpty(String.valueOf(pizzeria)))  {
-                return null;
-            }
-
-            String orderId = Helper.generateId();
-
-            Order order = new Order.Builder().setOrderId(orderId).setCreateDate(createDate).setTime(time).setCustomer(customer).setOrderStatus(orderStatus).setPizzeria(pizzeria).build();
-            return order;
-
-        }
-
-    }
+//    public class OrderFactory {
+//        /*
+//
+//        public static Order buildOrder(LocalDate createDate, LocalTime time, Customer customer, Order.OrderStatus orderStatus, Pizzeria pizzeria) {
+//            if (Helper.isNullOrEmpty(String.valueOf(Helper.isNullOrEmpty(String.valueOf(createDate)))) || Helper.isNullOrEmpty(String.valueOf(time))  || Helper.isNullOrEmpty(String.valueOf(customer)) || Helper.isNullOrEmpty(String.valueOf(orderStatus)) || Helper.isNullOrEmpty(String.valueOf(pizzeria)))  {
+//                return null;
+//            }
+//
+//            String orderId = Helper.generateId();
+//
+//            Order order = new Order.Builder().setOrderId(orderId).setCreateDate(createDate).setTime(time).setCustomer(customer).setOrderStatus(orderStatus).setPizzeria(pizzeria).build();
+//            return order;
+//
+//        }
+//
+//         */
+//
+//        public static Order createOrder(LocalDate createDate, LocalTime time, Customer customer, Order.OrderStatus orderStatus, Pizzeria pizzeria) {
+//            if (Helper.isNullOrEmpty(String.valueOf(Helper.isNullOrEmpty(String.valueOf(createDate)))) || Helper.isNullOrEmpty(String.valueOf(time))  || Helper.isNullOrEmpty(String.valueOf(customer)) || Helper.isNullOrEmpty(String.valueOf(orderStatus)) || Helper.isNullOrEmpty(String.valueOf(pizzeria)))  {
+//                return null;
+//            }
+//
+//            String orderId = Helper.generateId();
+//
+//            Order order = new Order.Builder().setOrderId(orderId).setCreateDate(createDate).setTime(time).setCustomer(customer).setOrderStatus(orderStatus).setPizzeria(pizzeria).build();
+//            return order;
+//
+//        }
+//
+//    }
 
