5b30d72bb7e3f2b5f7961ae7c2d2b0eab1a41493
DKamalie <46668318+DKamalie@users.noreply.github.com>
2023-10-14 16:49:58 +0200

    Fixed plus and minus buttons

--- src/main/java/com/example/application/views/menu/MenuView.java
+++ src/main/java/com/example/application/views/menu/MenuView.java
@@ -1,44 +1,31 @@
 package com.example.application.views.menu;
 
-import com.example.application.api.EmployeeApi;
 import com.example.application.api.MenuApi;
-import com.example.application.api.PizzaApi;
 import com.example.application.domain.*;
-import com.example.application.factory.PizzeriaFactory;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Html;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.AlignItems;
-import com.vaadin.flow.theme.lumo.LumoUtility.Display;
-import com.vaadin.flow.theme.lumo.LumoUtility.FontSize;
-import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
 import com.vaadin.flow.theme.lumo.LumoUtility.JustifyContent;
-import com.vaadin.flow.theme.lumo.LumoUtility.ListStyleType;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
-import com.vaadin.flow.theme.lumo.LumoUtility.MaxWidth;
-import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
-import com.vaadin.flow.theme.lumo.LumoUtility.TextColor;
 
-import java.text.DecimalFormat;
-import java.time.LocalDate;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @PageTitle("Menu")
 @Route(value = "menu", layout = MainLayout.class)
 public class MenuView extends Main implements HasComponents, HasStyle {
     private int count =0;
-
+    private Map<Pizza, Integer> pizzaCountMap = new HashMap<>();
+    private Map<Pizza, Paragraph> pizzaParapraphMap = new HashMap<>();
     Button order = new Button("Order");
     MenuApi getAll = new MenuApi();
     Div sideBar = new Div();
@@ -61,28 +48,70 @@ public class MenuView extends Main implements HasComponents, HasStyle {
             add(createPizzaSpan(pizza));
         });
         add(orderBar());
-        setSideBar();
+        //setSideBar();
     }
     public Div orderBar() {
         return sideBar;
     }
-    public void setSideBar(){
-        sideBar.addClickListener(clickEvent -> {
-            sideBar.addClassName("openSidebar");
-            System.out.println("Korean marmelade");
-        });
-    }
+//    public void setSideBar(){
+//        sideBar.addClickListener(clickEvent -> {
+//            sideBar.addClassName("openSidebar");
+//            System.out.println("Korean marmelade");
+//        });
+//    }
     private Div createPizzaSpan(Pizza pizza) {
-        order = new Button("add");
-        order.addClassName("orderBtn");
-        order.addClickListener(event -> {
-            System.out.println(pizza.getName());
-            this.totalPrice = this.totalPrice + pizza.getPrice();
-            System.out.println(totalPrice);
-            getPriceDiv.setText("Total: R" + totalPrice);
-            sideBar.add(new Paragraph(String.valueOf(pizza.getName())));
-        });
         Div pizzaContainer = new Div();
+        Button plus = new Button("+");
+        plus.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
+        plus.addClassName("plus");
+        Button minus = new Button("-");
+        minus.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        minus.addClassName("minus");
+        AtomicInteger pizzaCount = new AtomicInteger(0);
+
+        plus.addClickListener(event -> {
+           int count = pizzaCount.incrementAndGet();
+           updateCountAndPrice(pizza, count);
+            System.out.println(count);
+        });
+        minus.addClickListener(event -> {
+            int count = pizzaCount.decrementAndGet();
+            if (count >= 0) {
+                updateCountAndPrice(pizza, count);
+                System.out.println(count);
+            }
+        });
+
+
+
+        //order = new Button("Add");
+        //order.addClassName("orderBtn");
+
+
+//        order.addClickListener(event -> {
+//
+//            System.out.println(pizza.getName());
+//
+//            // Check if pizza in map already
+//            // Add count if not
+//            if(!pizzaCountMap.containsKey(pizza)) {
+//                pizzaCountMap.put(pizza, 1);
+//            }else {
+//                int count = pizzaCountMap.get(pizza);
+//                pizzaCountMap.put(pizza, count + 1);
+//                System.out.println(count);
+//            }
+//            // update total price
+//
+//            this.totalPrice = this.totalPrice + pizza.getPrice();
+//            System.out.println(totalPrice);
+//            getPriceDiv.setText("Total: R" + totalPrice);
+//            if (pizzaCountMap.get(pizza) <= 1) {
+//                sideBar.add(new Paragraph(String.valueOf(pizza.getName())));
+//            }
+//
+//        });
+
         pizzaContainer.addClassName("pizzaContainer");
         Div wholeCard = new Div();
         wholeCard.addClassName("wholeCard");
@@ -99,11 +128,69 @@ public class MenuView extends Main implements HasComponents, HasStyle {
                 )
         );
         wholeCard.add(span);
-        order.addClickListener(buttonClickEvent -> order.setId("testId"));
-        pizzaContainer.add(wholeCard, order );
+        //order.addClickListener(buttonClickEvent -> order.setId("testId"));
+        pizzaContainer.add(wholeCard );
         pizzaContainer.addClassNames(AlignItems.CENTER, JustifyContent.BETWEEN);
         pizzaContainer.addClassName("menuCard");
+        pizzaContainer.add(plus, minus);
         return pizzaContainer;
+    }
+
+//    private void updateCountAndPrice(Pizza pizza, int count) {
+//        pizzaCountMap.put(pizza, count);
+//        totalPrice = calculateTotalPrice();
+//        getPriceDiv.setText("Total: R" + totalPrice);
+//
+//        if(pizzaParapraphMap.containsKey(pizza)) {
+//            if(count == 0) {
+//                Paragraph paragraphToRemove = pizzaParapraphMap.get(pizza);
+//                sideBar.remove(paragraphToRemove);
+//                pizzaParapraphMap.remove(pizza);
+//            }
+//        }else {
+//            if (count > 0) {
+//                Paragraph newParagraph = new Paragraph(String.valueOf(pizza.getName()));
+//                sideBar.add(newParagraph);
+//                pizzaParapraphMap.put(pizza, newParagraph);
+//            }
+//        }
+//
+//        if(count == 0){
+//            sideBar.remove(new Paragraph(String.valueOf(pizza.getName())));
+//        }else if (count == 1){
+//            sideBar.add(new Paragraph(String.valueOf(pizza.getName())));
+//        }
+//    }
+private void updateCountAndPrice(Pizza pizza, int count) {
+    pizzaCountMap.put(pizza, count);
+    totalPrice = calculateTotalPrice();
+    getPriceDiv.setText("Total: R" + totalPrice);
+
+    if (count == 0) {
+        if (pizzaParapraphMap.containsKey(pizza)) {
+            Paragraph paragraphToRemove = pizzaParapraphMap.get(pizza);
+            sideBar.remove(paragraphToRemove);
+            pizzaParapraphMap.remove(pizza);
+        }
+    } else {
+        if (pizzaParapraphMap.containsKey(pizza)) {
+            // If the pizza paragraph already exists, update its text
+            Paragraph existingParagraph = pizzaParapraphMap.get(pizza);
+            existingParagraph.setText(pizza.getName() + " (x" + count + ")");
+        } else {
+            // If the pizza paragraph does not exist, create a new one and add it to the sidebar
+            Paragraph newParagraph = new Paragraph(pizza.getName() + " (x" + count + ")");
+            sideBar.add(newParagraph);
+            pizzaParapraphMap.put(pizza, newParagraph);
+        }
+    }
+}
+    private double calculateTotalPrice(){
+        double total = 0.0;
+        for (Map.Entry<Pizza, Integer> entry : pizzaCountMap.entrySet()){
+            total += entry.getKey().getPrice() * entry.getValue();
+        }
+        return total;
     }
     private Div createOrder() {
         Div btnContainer = new Div();
