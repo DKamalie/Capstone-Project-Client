75958a9f7f6abde7a2443f20414a22242c56e854
DKamalie <46668318+DKamalie@users.noreply.github.com>
2023-10-14 12:15:21 +0200

    Updated functionality for adding totals

--- src/main/java/com/example/application/views/menu/MenuView.java
+++ src/main/java/com/example/application/views/menu/MenuView.java
@@ -9,6 +9,7 @@ import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.*;
@@ -28,6 +29,7 @@ import com.vaadin.flow.theme.lumo.LumoUtility.MaxWidth;
 import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
 import com.vaadin.flow.theme.lumo.LumoUtility.TextColor;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Set;
@@ -36,42 +38,52 @@ import java.util.Set;
 @Route(value = "menu", layout = MainLayout.class)
 public class MenuView extends Main implements HasComponents, HasStyle {
     private int count =0;
-    Button order;
+
+    Button order = new Button("Order");
     MenuApi getAll = new MenuApi();
-    Div sideBar;
+    Div sideBar = new Div();
+    Div getPriceDiv = new Div();
+    double totalPrice;
+    Div priceDiv = new Div();
     public MenuView() {
+        H3 h = new H3("ORDER:");
+        getPriceDiv.addClassName("getPriceDiv");
+        getPriceDiv.setText("Total: R0");
+
+
+        sideBar.addClassName("sideBar");
+        sideBar.add(getPriceDiv);
+        sideBar.add(h);
         addClassName("mainContainer");
         Set<Pizza> pizzas = getAll.getAllPizzas();
         pizzas.forEach(pizza -> {
            count = count +1;
             add(createPizzaSpan(pizza));
         });
-        orderBar().add(createOrder());
         add(orderBar());
-
+        setSideBar();
     }
-
     public Div orderBar() {
-        sideBar = new Div();
-        Paragraph p = new Paragraph("ORDER:");
-        sideBar.add(p);
-        sideBar.addClassName("sideBar");
-        order.addClickListener(event -> {
-            System.out.println("TEST");
-        });
         return sideBar;
     }
-
+    public void setSideBar(){
+        sideBar.addClickListener(clickEvent -> {
+            sideBar.addClassName("openSidebar");
+            System.out.println("Korean marmelade");
+        });
+    }
     private Div createPizzaSpan(Pizza pizza) {
         order = new Button("add");
         order.addClassName("orderBtn");
         order.addClickListener(event -> {
             System.out.println(pizza.getName());
+            this.totalPrice = this.totalPrice + pizza.getPrice();
+            System.out.println(totalPrice);
+            getPriceDiv.setText("Total: R" + totalPrice);
             sideBar.add(new Paragraph(String.valueOf(pizza.getName())));
         });
         Div pizzaContainer = new Div();
         pizzaContainer.addClassName("pizzaContainer");
-        //pizzaContainer.addClassNames(AlignItems.CENTER, JustifyContent.BETWEEN, MaxWidth.SCREEN_LARGE, Margin.Horizontal.AUTO, Padding.Bottom.LARGE, Padding.Horizontal.LARGE);;
         Div wholeCard = new Div();
         wholeCard.addClassName("wholeCard");
         Span span = new Span(
@@ -88,24 +100,21 @@ public class MenuView extends Main implements HasComponents, HasStyle {
         );
         wholeCard.add(span);
         order.addClickListener(buttonClickEvent -> order.setId("testId"));
-        pizzaContainer.add(wholeCard, order);
-
-        HorizontalLayout container = new HorizontalLayout();
+        pizzaContainer.add(wholeCard, order );
         pizzaContainer.addClassNames(AlignItems.CENTER, JustifyContent.BETWEEN);
         pizzaContainer.addClassName("menuCard");
-        //add(container);
         return pizzaContainer;
     }
     private Div createOrder() {
         Div btnContainer = new Div();
         btnContainer.addClassName("btnContainer");
-        Div span = new Div(
-                new Paragraph("BISCUITS")
-        );
+        Div span = new Div();
+        span.setText("Total: R" + totalPrice);
         span.addClassName("test");
-        order.addClickListener(buttonClickEvent -> span.setText("TEST BISCUITS"));
+        order.addClickListener(buttonClickEvent -> span.setText("Total: R" + totalPrice));
         HorizontalLayout hl = new HorizontalLayout();
         hl.add(span);
+        //sideBar.add(span);
         return span;
     }
 }
