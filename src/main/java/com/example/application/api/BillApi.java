49b256a6e370ebcd337d4503d2d0d052f19eece5
keenan12390 <63111603+keenan12390@users.noreply.github.com>
2023-10-21 22:32:49 +0200

    Updated

--- src/main/java/com/example/application/api/BillApi.java
+++ src/main/java/com/example/application/api/BillApi.java
@@ -1,10 +1,17 @@
 package com.example.application.api;
 
 import com.example.application.domain.Bill;
+import com.vaadin.flow.server.VaadinSession;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -17,6 +24,16 @@ import java.util.Set;
  */
 @RestController
 public class BillApi {
+    private String token = "";
+    public String setToken(){
+        AuthenticateUserApi i = new AuthenticateUserApi();
+        token = i.getToken(
+                "bouali.social@gmail.com",
+                "password"
+        );
+
+        return token;
+    }
 
     public Integer billId = 0;
     public double totalBill = 0.0;
@@ -36,6 +53,31 @@ public class BillApi {
 
         return bill1;
     }
+
+    public Bill createBillAuth(Bill bill) throws IOException {
+        billId = bill.getBillId();
+        totalBill = bill.getTotalBill();
+        bill = null;
+
+        HttpEntity<Bill> request = new HttpEntity<>(new Bill(
+                billId,
+                totalBill
+        ));
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        HttpPost get = new HttpPost("http://localhost:8083/api/v1/auth/pizzeria/create");
+        get.setHeader("Authorization", "Bearer " + token);
+
+        HttpResponse response = httpclient.execute(get);
+        System.out.printf(Arrays.toString(response.getHeaders("Authorization")));
+        System.out.println(response.getStatusLine());
+        System.out.println("get:" + get);
+        System.out.println(response.getEntity());
+
+        return bill;
+    }
+
+
+
 
     public Bill readBill(Integer id){
         Bill billId = restTemplate.getForObject(urlBill + "read/" + id, Bill.class);
@@ -63,6 +105,22 @@ public class BillApi {
         String entityUrl = urlBill + "delete/" + id;
         System.out.println(entityUrl);
         restTemplate.delete(entityUrl);
+    }
+
+    public void deleteBillAuth(Integer billId) throws IOException {
+        String token = String.valueOf(VaadinSession.getCurrent().getAttribute("token"));
+        System.out.println("token: " +token);
+
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        HttpDelete get = new HttpDelete("http://localhost:8080/api/v1/auth/bill/delete/" + billId);
+        get.setHeader("Authorization", "Bearer " + token);
+
+        HttpResponse response = httpclient.execute(get);
+        System.out.printf(Arrays.toString(response.getHeaders("Authorization")));
+        System.out.println(response.getStatusLine());
+        System.out.println("get:" + get);
+        System.out.println(response.getEntity());
+
     }
     public Set<Bill> getAllBill() {
         String apiUrl = urlBill + "getAll";
